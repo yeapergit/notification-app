a66a20644715f4894c79fcc25ee4691dcf92ac96
Pedro Ferreira <yeaper@gmail.com>
2024-01-29 16:20:28 +0000

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <article>
+      <h2>The header</h2>
+      <p>This will be the header of this amazing app</p>
+    </article>
+  );
+}
+
+export default Header;
